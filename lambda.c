42a51ca86d2115ef9399fc55c04b13eaee7f5fd7
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2018-05-11 21:43:46 +0900

    Add reference to LAM

--- lambda.c
+++ lambda.c
@@ -9,6 +9,8 @@ enum nodetype {
 
 struct lambda_s {
   enum nodetype type;
+  struct lambda_s **refs[64];
+  int ref_num;
   char var;
   struct lambda_s *left;
   struct lambda_s *right;
@@ -23,6 +25,7 @@ lambda new_lambda() {
     fprintf(stderr, "Error: cannot alloc memory for lambda_s\n");
     exit(-1);
   }
+  new->ref_num = 0;
   new->var = 0;
   new->left = new->right = NULL;
   return new;
@@ -53,22 +56,25 @@ lambda app(lambda m, lambda n) {
 
 void print_lambda_r(lambda m) {
   switch (m->type) {
-    case VAR: putchar(m->var);
-              break;
-    case LAM: putchar('(');
-              putchar('\\');
-              putchar(m->var);
-              putchar('-');
-              putchar('>');
-              print_lambda_r(m->left);
-              putchar(')');
-              break;
-    case APP: putchar('(');
-              print_lambda_r(m->left);
-              putchar(' ');
-              print_lambda_r(m->right);
-              putchar(')');
-              break;
+  case VAR:
+    putchar(m->var);
+    break;
+  case LAM:
+    putchar('(');
+    putchar('\\');
+    putchar(m->var);
+    putchar('-');
+    putchar('>');
+    print_lambda_r(m->left);
+    putchar(')');
+    break;
+  case APP:
+    putchar('(');
+    print_lambda_r(m->left);
+    putchar(' ');
+    print_lambda_r(m->right);
+    putchar(')');
+    break;
   }
 }
 
@@ -77,11 +83,53 @@ void print_lambda(lambda m) {
   putchar('\n');
 }
 
+void derefer_r(lambda m, char c) {
+  int i;
+  switch (m->type) {
+  case VAR:
+    break;
+  case LAM:
+    m->var = c;
+    for (i = 0; i < m->ref_num; i++) {
+      *m->refs[i] = var(c);
+    }
+    derefer_r(m->left, c + 1);
+    break;
+  case APP:
+    derefer_r(m->left, c + 1);
+    derefer_r(m->right, c + 1);
+    break;
+  }
+}
+
+void derefer(lambda m) {
+  derefer_r(m, 'a');
+}
+
 int main(void) {
   lambda K = lam('x', lam('y', var('x')));
   lambda S = lam('x', lam('y', lam('z', app(app(var('x'), var('z')), app(var('y'), var('z'))))));
 
   print_lambda(K);
+  print_lambda(S);
+
+  lambda lx = S;
+  lambda ly = lx->left;
+  lambda lz = ly->left;
+  lambda ra = lz->left;
+
+  lx->ref_num = 1;
+  lx->refs[0] = &ra->left->left;
+
+  ly->ref_num = 1;
+  ly->refs[0] = &ra->right->left;
+
+  lz->ref_num = 2;
+  lz->refs[0] = &ra->left->right;
+  lz->refs[1] = &ra->right->right;
+
+  derefer(S);
+
   print_lambda(S);
 
   return 0;
